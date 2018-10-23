85bf59760dba31f42cde5ee20ed93fcd77eb4c5d
Kashif Ali <kashifali874@gmail.com>
2018-10-23 08:46:48 -0700

    Code refector, view did load function

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -25,10 +25,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstQuestion = allQuestions.list[0]
-        questionLabel.text = firstQuestion.questionText
-        
-        
+//        let firstQuestion = allQuestions.list[0]
+//        questionLabel.text = firstQuestion.questionText
+            nextQuestion()
     }
 
 
